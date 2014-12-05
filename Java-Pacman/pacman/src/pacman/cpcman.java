f509fdd567a4ab0d5a3eed2b8682dbe1e02067d8
Vladimir Ledovskoy <vsrl36@yandex.ru>
2014-12-05 18:52:34 +0300

    Create new project

---
+++ Java-Pacman/pacman/src/pacman/cpcman.java
@@ -0,0 +1,72 @@
+package pacman;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * main class
+ */
+public class cpcman  extends Frame
+implements Runnable, KeyListener, ActionListener, WindowListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
