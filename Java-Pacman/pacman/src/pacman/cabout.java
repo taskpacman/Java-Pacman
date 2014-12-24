bfea3b70020b378705d73c541f3104b4d79c0dfa
taskpacman <vsrl36@yandex.ru>
2014-12-24 22:07:49 +0300

    Небольшая доработка финальной версии

--- Java-Pacman/pacman/src/pacman/cabout.java
+++ Java-Pacman/pacman/src/pacman/cabout.java
@@ -1,28 +1,8 @@
-/**
- * Copyright (C) 1997-2010 Junyang Gu <mikejyg@gmail.com>
- * 
- * This file is part of javaiPacman.
- *
- * javaiPacman is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * javaiPacman is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with javaiPacman.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-package mikejyg.javaipacman.pacman;
+package pacman;
 
 import java.awt.*;
 import java.awt.event.*;
 
-// to display about information
 class cabout extends Window
 implements MouseListener
 {
@@ -30,12 +10,8 @@ implements MouseListener
 
 	final String[] about = {
 			"",
-			"javaiPacman",
-			"",
-			"  - Copyright 1997-2010 Junyang Gu <mikejyg@gmail.com>",
-			"",
-			"an intelligent pacman game implmented in Java",
-			""
+			"Pacman, copyright 2014, CSF, group 3.2"
+
 	};
 
 	cabout(Frame parent)
@@ -60,7 +36,6 @@ implements MouseListener
 	public void mouseClicked(MouseEvent e)
 	{
 		dispose();
-		// e.consume();
 	}
 
 	public void mousePressed(MouseEvent e) 
